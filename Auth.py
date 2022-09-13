c98e01f6ac8a505d3a15b1e70816a7656c845664
Gustavo Henrique da Rocha Reis <gustavo.reis@ifsudestemg.edu.br>
2022-09-12 22:02:29 -0300

    Adicionado comentários no código

--- Auth.py
+++ Auth.py
@@ -8,10 +8,12 @@ class Authentication():
     __user = ""
     __password = ""
 
+    # Recebe, como parâmetro, usuário e senha
     def __init__(self, user, password):
         self.__user = user
         self.__password = password
 
+    # Autentica o usuário
     def auth(self):
         # Retorna status do login
         __status = pam.authenticate(self.__user, self.__password)
